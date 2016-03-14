9e0120b4bf3fc803531cf145b945a0edf8b72dbb
Sander van Leeuwen <sanderl@mediamonks.com>
2016-03-14 16:27:35 +0100

    Add NS_API_BASIC_AUTH stub

--- src/dontbelate/settings.py
+++ src/dontbelate/settings.py
@@ -1,8 +1,10 @@
 from google.appengine.api import app_identity
 
 NS_DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
+NS_API_BASIC_AUTH = ('emailaddress', 'api-token')
 
 MIN_SLUG_LENGTH = 3
+
 
 # General
 APP_ID_PRODUCTION = 'dont-be-late'
